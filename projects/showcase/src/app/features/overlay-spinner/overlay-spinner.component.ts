a4e65d09ed6954a2f689bc15cf41c30320672c70
Andres Sandoval <andresandoval992@gmail.com>
2024-08-19 09:04:20 -0500

    Fix box-sizing on material-file-uploader

--- projects/showcase/src/app/features/overlay-spinner/overlay-spinner.component.ts
+++ projects/showcase/src/app/features/overlay-spinner/overlay-spinner.component.ts
@@ -14,10 +14,22 @@ import {OverlaySpinnerRef, OverlaySpinnerService} from '@entropia-io/ngx-overlay
 export class OverlaySpinnerComponent {
 
   private readonly overlaySpinnerService: OverlaySpinnerService = inject(OverlaySpinnerService);
+  public timeout: number = 3;
+  public showingOverlay: boolean = false;
+  public remainingTime: number = this.timeout;
 
   showSpinner(): void {
     const ref: OverlaySpinnerRef = this.overlaySpinnerService.show();
-    setTimeout(() => ref.hide(), 5000);
+    this.showingOverlay = true;
+    this.remainingTime = this.timeout;
+    const interval = setInterval(() => {
+      this.remainingTime -= 1;
+      if (this.remainingTime <= 0) {
+        clearInterval(interval);
+        this.showingOverlay = false;
+        ref.hide();
+      }
+    }, 1000);
   }
 
 }
